dab3850852cf53b80c3fc9e92e9692f4ba22fe75
Manuel Di Nardo <134846139+Ciomino@users.noreply.github.com>
2024-05-24 15:23:21 +0200

    Add files via upload

--- dbJavaProject/src/main/java/com/test/App.java
+++ dbJavaProject/src/main/java/com/test/App.java
@@ -1,32 +1,28 @@
 package com.test;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import com.test.dbService.UserDao;
 import com.test.model.User;
 
 public class App 
 {
     public static void main( String[] args ){
-        // User user = new User(10, "prova", "prova", "prova@prova.prova", 100);
-        UserDao dao = new UserDao();
-        User userNew = dao.getUserById(2);
-        userNew.versamento();
+        
     }
 
 
-    public static boolean login(String email, String password, ArrayList<User> userList) {
+    public static boolean login(String Username, String password, List<User> userList) {
         Scanner inLog = new Scanner(System.in);
-        int tentativiEmail = 0; // Contatore dei tentativi per l'email
+        int tentativiUsername = 0; // Contatore dei tentativi per l'Username
         int tentativiPassword = 0; // Contatore dei tentativi per la password
 
-        while (tentativiEmail < 3 || tentativiPassword < 3) { // Ciclo per i tentativi
-            boolean emailTrovata = false; // Flag per indicare se l'email è stata trovata
+        while (tentativiUsername < 3 || tentativiPassword < 3) { // Ciclo per i tentativi
+            boolean UsernameTrovata = false; // Flag per indicare se l'Username è stata trovata
 
             for (User user : userList) { // Ciclo per ogni User
-                if (email.equals(user.getEmail())) { // Se l'email corrisponde
-                    emailTrovata = true;
+                if (Username.equals(user.getUsername())) { // Se l'Username corrisponde
+                    UsernameTrovata = true;
 
                     if (password.equals(user.getPassword())) { // Se la password corrisponde
                         System.out.println("Benvenuto " + user.getUsername()); // Messaggio di benvenuto
@@ -37,8 +33,8 @@ public class App
 
                         if (riprovaPassword.equalsIgnoreCase("si")) { // Se l'User vuole riprovare
                             tentativiPassword++; // Incrementa il contatore dei tentativi per la password
-                            System.out.println("Reinserisci email e password:"); // Richiesta di email e password
-                            email = inLog.nextLine();
+                            System.out.println("Reinserisci Username e password:"); // Richiesta di Username e password
+                            Username = inLog.nextLine();
                             password = inLog.nextLine();
 
                             break; // Esce dal ciclo for per riprovare con le nuove credenziali
@@ -59,20 +55,20 @@ public class App
                     }
                 }
             }
-            if (!emailTrovata) {
-                System.out.println("L'email inserita non è corretta. Riprovare? SI/NO");
-                String riprovaEmail = inLog.nextLine();
-                if (riprovaEmail.equalsIgnoreCase("si")) {
-                    tentativiEmail++;
-                    System.out.println("Reinserisci email e password:");
-                    email = inLog.nextLine();
+            if (!UsernameTrovata) {
+                System.out.println("L'Username inserita non è corretta. Riprovare? SI/NO");
+                String riprovaUsername = inLog.nextLine();
+                if (riprovaUsername.equalsIgnoreCase("si")) {
+                    tentativiUsername++;
+                    System.out.println("Reinserisci Username e password:");
+                    Username = inLog.nextLine();
                     password = inLog.nextLine();
-                } else if (riprovaEmail.equalsIgnoreCase("no")) {
-                    System.out.println("Vuoi procedere con il recupero dell'email? SI/NO");
-                    riprovaEmail = inLog.nextLine();
-                    if (riprovaEmail.equalsIgnoreCase("si")) {
-                        // Chiamata alla funzione per il recupero dell'email
-                        System.out.println("Recupero email in corso");
+                } else if (riprovaUsername.equalsIgnoreCase("no")) {
+                    System.out.println("Vuoi procedere con il recupero dell'Username? SI/NO");
+                    riprovaUsername = inLog.nextLine();
+                    if (riprovaUsername.equalsIgnoreCase("si")) {
+                        // Chiamata alla funzione per il recupero dell'Username
+                        System.out.println("Recupero Username in corso");
                     } else {
                         System.out.println("CIAOOOOO!");
                         return false;
@@ -90,38 +86,38 @@ public class App
 
 
 
-    public static boolean registrazione(ArrayList<User> userList) {
+    public static boolean registrazione(List<User> userList) {
         Scanner inReg = new Scanner(System.in);
         Scanner inRegDoub = new Scanner(System.in);
-        System.out.println("Inserisci un'email");
-        String email = inReg.nextLine();
-        boolean emailEsistente = false;
+        System.out.println("Inserisci un'Username");
+        String Username = inReg.nextLine();
+        boolean UsernameEsistente = false;
     
-        // Controlla se l'email inserita esiste già tra gli utenti registrati
+        // Controlla se l'Username inserita esiste già tra gli utenti registrati
         for (User user : userList) {
-            if (user.getEmail().equals(email)) {
-                emailEsistente = true;
+            if (user.getUsername().equals(Username)) {
+                UsernameEsistente = true;
                 break;
             }
         }
     
-        // Se l'email esiste già, chiede all'User se vuole procedere con il login
-        if (emailEsistente) {
-            System.out.println("Email già esistente, vuoi procedere con il login? SI/NO");
+        // Se l'Username esiste già, chiede all'User se vuole procedere con il login
+        if (UsernameEsistente) {
+            System.out.println("Username già esistente, vuoi procedere con il login? SI/NO");
             String risposta = inReg.nextLine();
             if (risposta.equalsIgnoreCase("si")) {
-                // Se l'User vuole procedere con il login, richiede email e password
-                System.out.println("Inserisci email e password");
-                String emailLog = inReg.nextLine();
+                // Se l'User vuole procedere con il login, richiede Username e password
+                System.out.println("Inserisci Username e password");
+                String UsernameLog = inReg.nextLine();
                 String passLog = inReg.nextLine();
-                return login(emailLog, passLog, userList);
+                return login(UsernameLog, passLog, userList);
             } else {
                 // Se l'User non vuole procedere con il login, termina la registrazione
                 System.out.println("Deciditi, ciao!");
                 return false;
             }
         } else {
-            // Se l'email non esiste già, richiede password, username, numero e età per la registrazione
+            // Se l'Username non esiste già, richiede password, username, numero e età per la registrazione
             System.out.println("Inserisci password");
             String password = inReg.nextLine();
             while (password.isEmpty()) {
@@ -134,7 +130,7 @@ public class App
             double balance = inRegDoub.nextDouble();
     
             // Crea un nuovo oggetto User e lo aggiunge alla lista degli utenti registrati
-            User nuovoUser = new User(0, username, password, email, balance);
+            User nuovoUser = new User(0, username, password, Username, balance);
             userList.add(nuovoUser);
             System.out.println("User registrato con successo");
             return true;
